c11d7cfbb4f1a4ea7ee30519b3ae4ed5ce894fb2
comfouriertech <comfouriertech@comfouriertechdemac-mini.local>
2017-01-15 19:27:28 +0800

    Initial Commit

---
+++ CactusLeaderBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CactusLeaderBoard
+//
+//  Created by comfouriertech on 17/1/15.
+//  Copyright © 2017年 ronghua_li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CactusLeaderBoard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CactusLeaderBoard
+//
+//  Created by comfouriertech on 17/1/15.
+//  Copyright © 2017年 ronghua_li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
